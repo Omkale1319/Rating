a2674904d618144af6d28470636fdfaebd5a4a24
Om <kale9932@gmail.com>
2024-08-14 11:24:13 +0530

    Initial Commit

---
+++ Review 1/Review_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Review_1App.swift
+//  Review 1
+//
+//  Created by PTPL-MACM-01 on 14/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Review_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
